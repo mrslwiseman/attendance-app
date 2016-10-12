b036aa17b1efa310f7ac37ed1827d2fe4dd380fb
Joe Szili <laughingbubba@outlook.com>
2016-10-12 13:46:32 +1100

    fix routes fur sure

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -3,12 +3,12 @@ import { Router, Route, browserHistory } from 'react-router';
 
 // route components
 import AttendanceApp from '/imports/ui/layouts/AttendanceApp.js';
-import NotFoundPage from '/imports/ui/pages/NotFoundPage.js';
+import PageNotFound from '/imports/ui/pages/PageNotFound.js';
 import HomePage from '/imports/ui/pages/HomePage.js';
 
 export const renderRoutes = () => (
   <Router history={browserHistory}>
       <Route path="/" component={AttendanceApp}/>
-      <Route path="*" component={PagetNotFound}/>
+      <Route path="*" component={PageNotFound}/>
   </Router>
 );
